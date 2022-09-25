4c9def647b3f24887802d0d526f6809739ec6246
Aparna <aparna@metricsatsocial.in>
2022-09-25 20:38:28 +0530

    main layout created

--- metrics-social-ui/src/App.js
+++ metrics-social-ui/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import "./styles.css";
+import SideNavBar from "./components/sidenav";
+import HeaderBar from "./components/header-bar";
+import Cards from "./components/cards";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderBar />
+      <div className="container">
+        <SideNavBar />
+        <div className="card-layout">
+          <Cards />
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
